ea390dadb728aaac568de7ea54ce86aac3b79cc3
Athirson <athirsonarceus@gmail.com>
2022-12-02 08:42:13 -0300

    more stuff

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+	render(<App />)
+	const linkElement = screen.getByText(/learn react/i)
+	expect(linkElement).toBeInTheDocument()
+})
+
+test('render all elements', () => {
+	render(<App />)
+	const element = screen.getByText(/Click on a pokemon to see more info/i)
+	expect(element).toBeInTheDocument()
+})
